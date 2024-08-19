4f7898709e246adc9abbcc6859b2207c415c383d
bleachjade <bleachjade@hotmail.com>
2024-08-19 09:58:22 +0700

    [feat] add get user by id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,13 +11,23 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
+  async findById(id: number): Promise<User | undefined> {
+    return this.usersRepository.findOne({
+      where: { id },
+      relations: ['posts'],
+    });
+  }
+
   async findOneByUsername(username: string): Promise<User | undefined> {
     return this.usersRepository.findOne({ where: { username } });
   }
 
   async createUser(username: string, password: string): Promise<User> {
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = this.usersRepository.create({ username, password: hashedPassword });
+    const user = this.usersRepository.create({
+      username,
+      password: hashedPassword,
+    });
     return this.usersRepository.save(user);
   }
 
